fe5125b947c5ceaa287836715dfcb754ad1579e4
Mhayk Whandson <hi@mhayk.com>
2022-10-12 16:49:35 +0100

    feat: create Text story.

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -1,8 +1,53 @@
-import { Text } from './Text'
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from './Text'
 
 export default {
     title: 'Components/Text',
-    component: Text
+    component: Text,
+    args: {
+        children: 'Lorem ipsum.',
+        size: 'md'
+    },
+    argTypes: {
+        size: {
+            options: ['sm', 'md', 'lg'],
+            control: {
+                type: 'inline-radio'
+            }
+        }
+    }
+} as Meta<TextProps>;
+
+export const Default: StoryObj<TextProps> = {
+    args: {
+        size: 'md'
+    }
 }
 
-export const Default = {}
+export const Small: StoryObj<TextProps> = {
+    args: {
+        size: 'sm'
+    }
+}
+
+export const Large: StoryObj<TextProps> = {
+    args: {
+        size: 'lg'
+    }
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+    args: {
+        asChild: true,
+        children: (
+            <p>It's me Mario !</p>
+        )
+    },
+    argTypes: {
+        children: {
+            table: {
+                disable: true,
+            }
+        }
+    }
+}
